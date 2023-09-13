308fe907c1940870a85541738d7fdfe94c75e849
Tim <Gitdyup@users.noreply.github.com>
2023-09-13 15:19:31 +1000

    Update Sum & Time Intel Measures.cs

--- Sum & Time Intel Measures.cs
+++ Sum & Time Intel Measures.cs
@@ -1,10 +1,10 @@
 // For Tabular editor 3 - PBI Measures
 
-// Creates a SUM measure for every currently selected column and hide the column.
+/* Creates a SUM measure for every currently selected column and hide the column. */
     foreach(var c in Selected.Columns)
     {
         var newMeasure = c.Table.AddMeasure(
-            "Sum of " + c.Name,                    // Name
+            c.Name + " Total" ,                    // Name
             "SUM(" + c.DaxObjectFullName + ")",    // DAX expression
             c.DisplayFolder                        // Display Folder
         );
@@ -19,3 +19,55 @@
         c.IsHidden = true;
     }
 
+//
+
+/* Then run time intel script on the created measures */
+
+    var dateColumn = "'Dates'[Date]";
+
+    // Creates time intelligence measures for every selected measure:
+    foreach(var m in Selected.Measures) {
+        // Year-to-date:
+        m.Table.AddMeasure(
+            m.Name + " YTD",                                       // Name
+            "TOTALYTD(" + m.DaxObjectName + ", " + dateColumn + ")",     // DAX expression
+            m.DisplayFolder                                        // Display Folder
+        );
+        
+        // Previous year:
+        m.Table.AddMeasure(
+            m.Name + " PY",                                       // Name
+            "CALCULATE(" + m.DaxObjectName + ", SAMEPERIODLASTYEAR(" + dateColumn + "))",     // DAX expression
+            m.DisplayFolder                                        // Display Folder
+        );    
+        
+        // Year-over-year
+        m.Table.AddMeasure(
+            m.Name + " YoY",                                       // Name
+            m.DaxObjectName + " - [" + m.Name + " PY]",            // DAX expression
+            m.DisplayFolder                                        // Display Folder
+        );
+        
+        // Year-over-year %:
+        m.Table.AddMeasure(
+            m.Name + " YoY%",                                       // Name
+            "DIVIDE([" + m.Name + " YoY], [" + m.Name + " PY])",    // DAX expression
+            m.DisplayFolder                                         // Display Folder
+        ).FormatString = "0.0 %";                                   // Set format string as percentage
+        
+        // Quarter-to-date:
+        m.Table.AddMeasure(
+            m.Name + " QTD",                                            // Name
+            "TOTALQTD(" + m.DaxObjectName + ", " + dateColumn + ")",    // DAX expression
+            m.DisplayFolder                                             // Display Folder
+        );
+        
+        // Month-to-date:
+        m.Table.AddMeasure(
+            m.Name + " MTD",                                       // Name
+            "TOTALMTD(" + m.DaxObjectName + ", " + dateColumn + ")",     // DAX expression
+            m.DisplayFolder                                        // Display Folder
+        );
+    }
+
+//
